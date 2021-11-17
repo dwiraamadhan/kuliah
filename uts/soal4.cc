876dd42833653eb4aa398f0e8d978e2ca63bae32
root <root@DESKTOP-6NQHQFM.localdomain>
2021-11-18 01:59:11 +0700

    Jawaban_UTS

---
+++ uts/soal4.cc
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Berarti usia kalian sekarang adalah 21 tahun" << endl;
+    return 0;
+}
---
+++ uts/soal5.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Masukkan angka pertama : ";
+    cin >> a;
+    cout << "Masukkan angka kedua : ";
+    cin >> b;
+    cout << "Penjumlahan : " << a+b << endl;
+    cout << "Pengurangan : " << a-b <<endl;
+    cout << "Perkalian : " << a*b <<endl;
+    cout << "Pembagian : " << a/b <<endl;
+
+    return 0;
+}
